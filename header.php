df59f20d5c1dc2bd7a7edb0a671cf1b33ec53120
easier <fysian@gmail.com>
2014-04-12 13:59:30 +0800

    first test

---
+++ header.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<!--[if IE 8 ]><html class="ie no-js" xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US"> <![endif]-->
+<!--[if IE 9]> <html class="ie9 no-js" xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html  class="no-js" xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US"><!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+	<title>Form. Responsive HTML5 template.</title>
+	<meta name="description" content="Form. Responsive HTML5 template.">
+	<meta name="keywords" content="form, responsive, multi-purpose, portfolio, parallax, template, html5, agency">
+	<meta name="author" content="Aether Themes">
+
+	<link rel="shortcut icon" href="images/favicon.png">
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!-- jQuery -->
+	<script src="js/jquery.js" type="text/javascript" charset="utf-8"></script>
+
+	<!-- Fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Lato:300,400|Montserrat' rel='stylesheet' type='text/css'>
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" type="text/css" href="css/framework.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+
+<body>
+	<!-- Begin Navigation -->
+	<nav class="mobile">
+
+		<!-- Search -->
+		<div class="search-trigger"></div>
+		<div class="search-form disabled">
+			<form action="404.html">
+				<input name="search" id="search" type="text" class="search" onFocus="if(this.value == 'Search...') { this.value = ''; }" onBlur="if(this.value == '') { this.value = 'Search...'; }" value="Search..." />
+			</form>
+		</div><!-- END -->
+
+		<!-- Menu -->
+		<ul class="nav-content clearfix">
+			<li id="magic-line"></li>
+			<li class="current-page drop first upper">
+				<a class="drop-btn" href="#">Home</a>
+				<ul class="drop-list">
+					<li><a href="index.html">Standard1</a></li>
+					<li><a href="home-alternative.html">Alternative</a></li>
+					<li><a href="home-portfolio-galore.html">Portfolio Galore</a></li>
+				</ul>
+			</li>
+			<li class="upper"><a href="about.html">About</a></li>
+			<li class="drop upper">
+				<a class="drop-btn" href="#">Work</a>
+				<ul class="drop-list">
+					<li><a href="portfolio.html">Portfolio</a></li>
+					<li><a href="portfolio-gallery.html">Gallery</a></li>
+					<li><a href="single-project.html">Single Project</a></li>
+				</ul>
+			</li>
+			<li class="upper"><a href="blog.html">Journal</a></li>
+			<li class="upper"><a href="contact.html">Contact</a></li>
+			<li class="upper"><a href="http://www.weidea.net">More theme</a></li>
+		</ul><!-- END -->
+
+	</nav>
+	<header class="mobile">
+
+		<!-- Logo -->
+		<a href="index.html"><img class="logo" src="images/logo.png" alt="Form Logo" width="96" height="35" /></a>
+
+		<!-- Menu Button -->
+		<button type="button" class="nav-button">
+		  <div class="button-bars"></div>
+	    </button><!-- END -->
+
+	</header>
+
+	<div class="sticky-head"></div>
+	<!-- End Navigation -->
+
+
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+	get_header();
+?>
+
+first test
+
+<?php
+	get_footer();
+?>
